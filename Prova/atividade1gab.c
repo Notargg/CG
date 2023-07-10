f9f64da1a9f62933ae130f672e72ef769d8a5652
Notargg <gabriel.graton@estudante.ufscar.br>
2023-07-10 14:23:42 -0300

    Mandar o gabarito
    Unica diferença - dy = y2 - y1

--- Prova/atividade1gab.c
+++ Prova/atividade1gab.c
@@ -1,16 +1,3 @@
-/*
-Atividade Realizada por:
-
-Gabriel Lourenço de Paula Graton - 800432
-
-Pedro Cassiano Coleone - 793249
-
-
-*/
-
-
-
-
 #include "cg2d.h"
 
 int desenha_linha_1(int x1, int y1, int x2, int y2, int cor, bufferdevice * monitor) { 
@@ -76,75 +63,57 @@ int desenha_linha_2(int x1, int y1, int x2, int y2, int cor, bufferdevice * moni
  
  Erro inicial: p = F(x+1,y-(1/2))
  
- Se p <= 0, modifica-se p = p + incremento e o próximo ponto será (x+1,y-1).
- O incremento é determinado avaliando-se a função 2*F(x+1,y-1).
+ Se p <= 0, modifica-se p = p + 2*dy + 2*dx e o próximo ponto será (x+1,y-1).
+ O incremento 2*dy + 2*dx é determinado avaliando-se a função 2*F(x+1,y-1).
  
- Se p > 0, modifica-se p = p + incremento e o próximo ponto será (x+1,y).
- O incremento é determinado avaliando-se a função 2*F(x+1,y).   
+ Se p > 0, modifica-se p = p + 2*dy e o próximo ponto será (x+1,y).
+ O incremento 2*dy é determinado avaliando-se a função 2*F(x+1,y).   
 */
 
-  printf("Implemente essa função!\n");    // É PARA TRABALHAR NESTE TRECHO!!!!!
-    
   int x, y, dx, dy, p;
 
-  // Como o valor do y2 vai ser (y-1), logo y1 vai ser maior que y1
-  // Diferente do Gabarito dy = y2 - y1
-
-  dy = y1 - y2;
-
-  // dx permanesce o mesmo
-
+  dy = y2 - y1;
   dx = x2 - x1;
  
   x = x1;
   y = y1;
-
-  // O erro é calculado como sendo 2*F(x+1,y-(1/2)) - Logo
-  // 2*(dyx + dy - dx + 1/2dx + bdx)
-  // 2*(dyx-dx+bdx) + 2(dy + 1/2dx)
-  // O primeiro termo é igual a F(x,y) que é = 0
-  // Já o segundo termo é igual a p, o erro
-  // 2dy + dx
-
+  
+  // Determinação do erro inicial
+  // F(x+1,y-(1/2)) = dy*(x+1) - dx*(y-(1/2)) + dx*b
+  //                = dy*x + dy - dx*y + dx/2 + dx*b
+  //                = dy*x - dx*y + dx*b + dy + dx/2
+  //                = F(x,y) + dy + dx/2
+  // Por definição, (x,y) são as coordenadas de uma entrada no SRD que corresponde a um ponto
+  // sobre a reta no SRU. Portanto, F(x,y) = 0 e F(x+1,y-(1/2)) = dy + dx/2
+  // Conforme exposto no enunciado da atividade, é comum utilizarmos p = 2*F(x+1,y-(1/2))
+  // Portanto, assumimos que p = 2*F(x+1,y-(1/2)) = 2*(dy + dx/2) = 2*dy + dx
   p = 2*dy + dx;
   
-  while ( (x <= x2) && (y >= y2) ) { // Como dito anteriormente, y1 vai ser maior que y2, logo precisa inverter de y <= y2 para y >= y2 
-    if (p >= 0) {
+  while ( (x <= x2) && (y >= y2) ) {
+    if (p <= 0) {
       monitor->buffer[(monitor->MaxY - y) * monitor->MaxX + x] = cor;
-
-      // Aqui o p será determinado por 2 *F(x+1,y-1) determinado anteriormente
-      // 2*(dyx + dy - dx + 1dx + bdx)
-      // 2*(dyx-dx+bdx) + 2(dy + dx)
-      // 2dy + 2dx
-
-      p = p + 2*dy + 2*dx; // Ficar com o sinal invertido
+      // Determinação do incremento para p <= 0
+      // 2*F(x+1,y-1) = 2*(dy*(x+1) - dx*(y-1) + dx*b)
+      //              = 2*dy*x + 2*dy - 2*dx*y + 2*dx + 2*dx*b
+      //              = 2*F(x,y) + 2*dy + 2*dx
+      // Da mesma forma, F(x,y) = 0 e portanto 2*F(x+1,y-1) = 2*dy + 2*dx será o incremento     
+      p = p + 2*dy + 2*dx;
       x = x + 1;
       y = y - 1; 
       }
     else {
       monitor->buffer[(monitor->MaxY - y) * monitor->MaxX + x] = cor;
-      p = p + 2*dy; // Aqui não muda pois é o caso onde 2 * F(x+1,y), sendo igual ao primeiro caso
+      // Determinação do incremento para p > 0
+      // 2*F(x+1,y)) = 2*(dy*(x+1) - dx*y + dx*b)
+      //             = 2*dy*x + 2*dy - 2*dx*y + 2*dx*b
+      //             = 2*F(x,y) + 2*dy
+      // Da mesma forma, F(x,y) = 0 e portanto 2*F(x+1,y) = 2*dy será o incremento        
+      p = p + 2*dy;
       x = x + 1;
       }
     }
     
   return 0;
-
-
-  /*
-    É possível desenhar um triângulo equilátero diretamente no SRD. Comente sua resposta no código.
-
-    Pensando somente com o uso das duas funções não é possível desenhar um triângulo equilátero diretamente no SRD, uma vez que
-    ao pensarmos na abordagem das funções, com o angulo variando de 0 a 1 e -1 e intervalo aberto de 0, a questão fica na matemática.
-    Triângulo Equiláteros possuem ângulo de 60 graus em seus vertices, mas as funções presentes nesse código só vão até 45°
-    Dado que, a - como coeficiente angular - é calculado como tg da reta, ao colocar o interavalo variando de 1 até 0
-    que é exatamente tg de 45 graus. logo não há como fechar o triângulo
-
-  
-  
-  */
-
-
   }
 
 int main(int argc, char ** argv) {
@@ -160,12 +129,12 @@ int main(int argc, char ** argv) {
   monitor = CreateBuffer(600,600);
 
   // Teste primeiro com esses valores para a desenha_linha_1()
-  //x1 = 20; y1 = 20;
-  //x2 = -580; y2 = 570;
+  x1 = 20; y1 = 20;
+  x2 = 580; y2 = 570;
 
-  // Depois, teste com esses valores para a desenha_linha_2()   
-  x1 = 20; y1 = 570;
-  x2 = 580; y2 = 20;  
+  // Depois, teste com esses valores para a desenha_linha_2()  
+  //x1 = 20; y1 = 570;
+  //x2 = 580; y2 = 20;  
   
   float a = (float) (y2-y1)/(x2-x1);
   cor = 1;
@@ -183,8 +152,6 @@ int main(int argc, char ** argv) {
       printf("O coeficiente angular está no intervalo [-1,0).\n");
       printf("Desenhando a linha.\n");
       desenha_linha_2(x1,y1,x2,y2,cor,monitor);
-  
-      
       Dump2X(monitor,palheta);
       }
     else printf("O coeficiente angular não está nos intervalos [-1,0) ou [0,1].\n");
@@ -192,5 +159,12 @@ int main(int argc, char ** argv) {
 
   return 0;
   }
+
+// Apenas com as funções desenha_linha_1() e desenha_linha_2() não é possível desenhar um triângulo equilátero.
+// A partir de um ponto dado, podemos gerar dois segmentos de reta que definem um arco de 60º.
+// Contudo, para a determinação de um terceiro segmento de reta que intersecta as duas outras retas em que os 
+// ângulos internos sejam iguais, temos que trabalhar com coeficientes angulares maiores do que 1 e menores do que -1.
+// Esses dois casos, m > 1 e m < -1 poderiam ser implementados em outras funções simplesmente considerando a simetria
+// nas equações y = x e y = -x.
 
 
